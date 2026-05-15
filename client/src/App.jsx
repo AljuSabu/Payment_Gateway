8aa2458c50644efa94261effad7a3ee7d59ab843
Alju Sabu <aljusabu555@gmail.com>
2026-05-15 15:38:29 +0530

    scroll to top functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Products from "./pages/Products";
 import About from "./pages/About";
 import PageNotFound from "./pages/PageNotFound";
 import SuccessPage from "./pages/SuccessPage";
+import ScrollToTop from "./components/utils/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
